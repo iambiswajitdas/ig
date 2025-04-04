f1900136185f11a9a089eb8ec292c41a1e0a6c16
iambiswajitdas <56880461+iambiswajitdas@users.noreply.github.com>
2025-04-04 13:24:17 +0530

    Update app.js - added like feature

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ buttons.forEach(btn => {
 function setServer(val) {server = val}
 
 async function follow() {
-    output.innerHTML = ''
+    output.innerHTML = `<p>Started Server${server}</p>`
 
     let username = document.getElementById('user').value
     let password = document.getElementById('pass').value
@@ -27,6 +27,36 @@ async function follow() {
     }
 
     const eventSource = new EventSource(`/follow/?server=${server}&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&instaID=${encodeURIComponent(instaID)}`)
+
+    eventSource.onmessage = (event) => {
+        output.innerHTML += `<p>${event.data}</p>`;
+    }
+
+    eventSource.onerror = (error) => {
+        console.log(error)
+        output.innerHTML += `<p>Some error occured. Try again...</p>`;
+        eventSource.close()
+    }
+
+    eventSource.addEventListener('done', () => {
+        output.innerHTML += `<p>:)</p>`
+        eventSource.close()
+    })
+}
+
+async function like() {
+    output.innerHTML = `<p>Started Server${server}</p>`
+
+    let username = document.getElementById('user').value
+    let password = document.getElementById('pass').value
+    let postLink = document.getElementById('post').value
+
+    if (!username || !password || !postLink) {
+        output.innerHTML += '<p>Please enter both username and password and Link</p>';
+        return;
+    }
+
+    const eventSource = new EventSource(`/like/?server=${server}&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&postLink=${encodeURIComponent(postLink)}`)
 
     eventSource.onmessage = (event) => {
         output.innerHTML += `<p>${event.data}</p>`;
